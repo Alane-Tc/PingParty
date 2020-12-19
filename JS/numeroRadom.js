db493ba42066176bb4985420f91e222ee784ea51
Alane-Tc <alantamez28@hotmail.com>
2020-12-18 23:13:03 -0600

    fixed bug

--- JS/numeroRadom.js
+++ JS/numeroRadom.js
@@ -8,11 +8,15 @@ window.addEventListener("load", function () {
 
     let show = () => { showResults.style.display = "block"; }
 
+    let hideResults = () => { showResults.style.display = "none"; }
+
     let Remove = () => { window.location = "numeroRandom.html"; }
 
     let numberRandom = (number1, number2) => { return Math.round(Math.random() * (number2 - number1) + parseInt(number1)); }
 
     let showAlertError = () => { showAlert.style.display = "block"; }
+
+    let hideAlertError = () => { showAlert.style.display = "none"; }
 
     let imageSad = () => { showImage.innerHTML = '<img src="Img/sad-error.png" alt="Sad" width="110px" height="110px">' }
 
@@ -28,10 +32,12 @@ window.addEventListener("load", function () {
 
         if (n1 < 0 || n2 < 0) {
             numberNegative();
+            hideResults();
         } else if (isNaN(n1) || isNaN(n2)) {
             alertaError();
         } else {
             show();
+            hideAlertError();
             let results = numberRandom(n2, n1);
             document.querySelector("#span-results").innerHTML = results;
         }
