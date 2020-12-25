02e814fdda188e2901ce47ae2d80340c2751008a
Alane-Tc <alantamez28@hotmail.com>
2020-12-24 18:42:39 -0600

    add message error

--- JS/cooperacha.js
+++ JS/cooperacha.js
@@ -4,6 +4,7 @@ window.addEventListener("load", function () {
     var btnCalculate = document.querySelector("#btn-Calculate");
     var btnClear = document.querySelector("#btn-Clear");
     var resultsCooperacha = document.querySelector("#result-Cooperacha");
+    var alertError = document.querySelector("#alert-Error")
 
     let Clear = () => { window.location = "cooperacha.html" }
 
@@ -11,20 +12,23 @@ window.addEventListener("load", function () {
         var person = parseInt(document.querySelector("#input-Person").value);
         let Total = 0;
         let n1
-        for (let i = 1; i <= person; i++) {
-            n1 = prompt("¿Cuál es el precio del producto?: " + (i));
-            Total = Total + parseInt(n1);
+        if (isNaN(person)) {
+            alertError.innerHTML = `<h3>¡Uy! Algo salió mal</h3>`
+        } else if (person < 0) {
+            alertError.innerHTML = `<h3>No se pueden digitar numeros negativos</h3>`
+        } else {
+
+            for (let i = 1; i <= person; i++) {
+                n1 = prompt("¿Cuál es el precio del producto?: " + (i));
+                Total = Total + parseInt(n1);
+            }
+            resultsCooperacha.innerHTML = `La suma es: <span class="span-Total"><b>${Total}</b></span> ` +
+                '<br>' + `Cooperacha: <span class="span-Total"><b>${Total / person}</b></span> `
         }
-        resultsCooperacha.innerHTML = `La suma es: <span class="span-Total"><b>${Total}</b></span> ` +
-            '<br>' + `Cooperacha: <span class="span-Total"><b>${Total / person}</b></span> `
     }
 
-    btnCalculate.addEventListener("click", function () {
-        calculate();
-    });
+    btnCalculate.addEventListener("click", function () { calculate(); });
 
-    btnClear.addEventListener("click", function () {
-        Clear();
-    })
+    btnClear.addEventListener("click", function () { Clear(); })
 
 });
