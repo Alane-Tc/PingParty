3a2686b75b70caf22a7198032f4bed094845dd53
Alane-Tc <alantamez28@hotmail.com>
2020-12-28 10:23:50 -0600

    modified file

--- JS/cooperacha.js
+++ JS/cooperacha.js
@@ -14,15 +14,17 @@ window.addEventListener("load", function () {
         let n1
         if (isNaN(person)) {
             alertError.innerHTML = `<h3>¡Uy! Algo salió mal</h3>`
-        } else if (person < 0) {
+        } else if (person <= 0) {
             alertError.innerHTML = `<h3>No se pueden digitar numeros negativos</h3>`
+        } else if (person == 1) {
+            alertError.innerHTML = `<h3>Para sacar la cooperacha tienen que ser más de 2 productos</h3>`
         } else {
             for (let i = 1; i <= person; i++) {
                 n1 = prompt("¿Cuál es el precio del producto?: " + (i));
                 Total = Total + parseInt(n1);
             }
             resultsCooperacha.innerHTML = `La suma es: <span class="span-Total"><b>${Total}</b></span> ` +
-                '<br>' + `Cooperacha: <span class="span-Total"><b>${Total / person}</b></span> `
+                '<br>' + `Cooperacha de ${person} personas: <span class="span-Total"><b>${Total / person}</b></span> `
         }
     }
 
